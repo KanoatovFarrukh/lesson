ab9b4996781ecd10ebdc4a8facdc1e56b816dcb1
Kanoatov Farrukh <144346859+KanoatovFarrukh@users.noreply.github.com>
2023-09-11 07:37:58 +0500

    1 lesson
    python

---
+++ 1 dars.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('assalom aleykum Shohim')
+#Bu chop etish buyrug'i hisoblanadi.
+print  ()
+print('salom hammaga')
+print (18+2*5/2.5)
+print(5-3+9-1)
+print('assalom aleykun Shohim')
+
